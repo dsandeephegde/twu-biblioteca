12b6030542f6c329bd8bc2b9ccd635233d64ddda
dsandeephegde <sandeeph@thoughtworks.com>
2015-09-04 10:49:27 +0530

    display list of books

--- src/com/thoughtworks/biblioteca/Display.java
+++ src/com/thoughtworks/biblioteca/Display.java
@@ -1,9 +1,23 @@
 //Used to display to the output
 package com.thoughtworks.biblioteca;
 
+import java.util.ArrayList;
+
 public class Display {
 
-    public String Welcome() {
-        return "Welcome to Biblioteca";
+    ArrayList<String> books;
+
+    Display(ArrayList<String> books) {
+        this.books = books;
+    }
+
+    public void display(String message) {
+        System.out.println(message);
+    }
+
+    public void displayBooks() {
+        for (String book : books) {
+            display(book);
+        }
     }
 }
--- test/com/thoughtworks/biblioteca/DisplayTest.java
+++ test/com/thoughtworks/biblioteca/DisplayTest.java
@@ -1,14 +1,46 @@
 package com.thoughtworks.biblioteca;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class DisplayTest {
-    @Test
-    public void shouldReturnWelcomeMessage() {
-        Display display = new Display();
 
-        assertEquals("Welcome to Biblioteca", display.Welcome());
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+
+    @Test
+    public void shouldPrintWelcomeMessage() {
+        Display display = new Display(new ArrayList<String>());
+        display.display("Welcome to Biblioteca");
+        assertEquals("Welcome to Biblioteca\n", outContent.toString());
+    }
+
+    @Test
+    public void shouldPrintListOfBooks() {
+        ArrayList<String> books = new ArrayList<String>();
+        books.add("Book1");
+        books.add("Book2");
+        Display display = new Display(books);
+        display.displayBooks();
+
+        assertEquals("Book1\nBook2\n", outContent.toString());
     }
 }
